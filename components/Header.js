d8fbf92cf54094c28f7fb3b72c47029fc6601bf0
Joe Hannis <joe_hannis@hotmail.com>
2023-10-12 16:15:04 +0100

    icon adjustment

--- components/Header.js
+++ components/Header.js
@@ -6,27 +6,31 @@ import linkedInIcon from '../public/linkedin-logo-white.png';
 export default function Header() {
   return (
     <div className='header'>
-      <div className='box-container'>
-        <Link className='navbar-link' href='/'>
-          Home
-        </Link>
-        <Link className='navbar-link' href='/about'>
-          About
-        </Link>
-        <Link className='navbar-link' href='/projects'>
-          Projects
-        </Link>
+      <Link className='navbar-link' href='/'>
+        Home
+      </Link>
+      <Link className='navbar-link' href='/about'>
+        About
+      </Link>
+      <Link className='navbar-link' href='/projects'>
+        Projects
+      </Link>
 
-        <a href='https://github.com/joehannis' className='github-box'>
-          <Image className='icon' src={githubIcon} alt='GitHub icon' />
-        </a>
-        <a
-          href='https://www.linkedin.com/in/joe-hannis/'
-          className='linkedin-box'
-        >
-          <Image className='icon' src={linkedInIcon} alt='LinkedIn icon' />
-        </a>
-      </div>
+      <a href='https://github.com/joehannis'>
+        <div className='icon'>
+          <Image src={githubIcon} alt='GitHub icon' width={40} height={40} />
+        </div>
+      </a>
+      <a href='https://www.linkedin.com/in/joe-hannis/'>
+        <div className='icon'>
+          <Image
+            src={linkedInIcon}
+            alt='LinkedIn icon'
+            width={40}
+            height={40}
+          />
+        </div>
+      </a>
     </div>
   );
 }
